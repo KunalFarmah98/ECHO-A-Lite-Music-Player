07bbdde1502c5778f828abefac2925387ff463e2
Kunal Farmah <kunalfarmah98@gmail.com>
2026-08-20 00:27:05 +0530

    fixed top padding in edge to edge on song playing screen and navigation color

--- app/src/main/java/com/apps/kunalfarmah/echo/activity/SongPlayingActivity.kt
+++ app/src/main/java/com/apps/kunalfarmah/echo/activity/SongPlayingActivity.kt
@@ -3,14 +3,11 @@ package com.apps.kunalfarmah.echo.activity
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
-import android.util.TypedValue
 import android.view.View
-import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updateLayoutParams
 import com.apps.kunalfarmah.echo.R
 import com.apps.kunalfarmah.echo.databinding.ActivitySongPlayingBinding
 import com.apps.kunalfarmah.echo.fragment.SongPlayingFragment
@@ -32,27 +29,13 @@ class SongPlayingActivity : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_queue_music_white_24dp)
         supportActionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.colorPrimary)))
 
-        val typedValue = TypedValue()
-        var actionBarHeight = 0
-        if (theme.resolveAttribute(R.attr.actionBarSize, typedValue, true)) {
-            actionBarHeight = TypedValue.complexToDimensionPixelSize(typedValue.data, resources.displayMetrics)
-        }
-        // Fallback if attribute not found (though unlikely with standard themes)
-        if (actionBarHeight == 0 && supportActionBar != null) {
-            actionBarHeight = supportActionBar!!.height // Only if action bar is already measured
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM){
+            val topPadding = (50 * resources.displayMetrics.density).toInt()
+            binding.container.setPadding(0, topPadding, 0, 0)
         }
 
-        // handling edge to edge in Android 15+
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM){
-            // Apply top margin to the container if actionBarHeight is available
-            if (actionBarHeight > 0) {
-                binding.container.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                    topMargin = actionBarHeight
-                }
-            }
-        }
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, windowInsets ->
-            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout())
             view.setPadding(
                 systemBars.left,
                 systemBars.top,
